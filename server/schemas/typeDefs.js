9b0dbd0ec15674a0d73d088071ee8d998aa24d73
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-03-05 21:11:32 -0700

    Schema added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,16 +6,17 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
-    thoughts: [Thought]!
   }
 
-  type Thought {
-    _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
-  }
+  // type Animal {
+  //   _id: ID
+  //   animalType: String
+  //   animalBreed: String
+  //   animalName: String
+  //   animalAge: Int
+  //   createdAt: String
+  //   comments: [Comment]
+  // }
 
   type Comment {
     _id: ID
@@ -32,6 +33,8 @@ const typeDefs = gql`
   type Query {
     users: [User]
     user(username: String!): User
+    animals: [Animals]
+    animal: (animalType: String, animalBreed: String, animalName: String, animalAge: Int, id: ID)
     thoughts(username: String): [Thought]
     thought(thoughtId: ID!): Thought
     me: User
@@ -40,8 +43,10 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    addAnimal(animalType: String!, animalBreed: String!, animalName: String!, animalAge: Int!)
     addThought(thoughtText: String!): Thought
     addComment(thoughtId: ID!, commentText: String!): Thought
+    removeAnimal(animalType: String, animalBreed: String, animalName: String, animalAge: Int, animalId: ID!)
     removeThought(thoughtId: ID!): Thought
     removeComment(thoughtId: ID!, commentId: ID!): Thought
   }
