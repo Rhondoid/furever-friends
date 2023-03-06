114772c911a08cfc19b4b1e6ddea1f08acf964ec
Alec S <asummers34@gmail.com>
2023-03-05 21:36:46 -0700

    fixing package.json

--- server/server.js
+++ server/server.js
@@ -1,12 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-
-const PORT = process.env.PORT || 3001;
-
-const app = express()
-
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
-=======
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
@@ -49,4 +41,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
->>>>>>> 14ed33205744f3cd4df1c3b1fa1c1c28fa096a94
